a2be4ef60489f74be65bbf567420ceceddf12e78
MounikaBadarawada <143426183+MounikaBadarawada@users.noreply.github.com>
2024-06-21 12:52:12 +0530

    todo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoList from './Todolist'; // Adjust the path if TodoList.js is in a different directory
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App
